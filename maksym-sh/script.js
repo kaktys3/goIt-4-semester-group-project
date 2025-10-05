89ea1a40af4a2863dbeeb3be90e1b6d50a29b366
Maksym Shamrai <150514219+maksymshamrai@users.noreply.github.com>
2025-10-05 10:01:19 +0200

    css fix

--- maksym-sh/script.js
+++ maksym-sh/script.js
@@ -1,4 +1,4 @@
-import { InfoRavenclawAndCharactersHouse } from "../maksym-sh/api.js";
+import { InfoRavenclawAndCharactersHouse } from "./api.js";
 
 const studentsContainer = document.querySelector(".students-cards");
 
@@ -9,14 +9,32 @@ async function renderRavenclawStudents() {
         const studentsCard = document.createElement("div");
         studentsCard.classList.add("students-card");
         studentsCard.innerHTML = `
-            <img src="${student.image || 'https://via.placeholder.com/150'}" alt="${student.name}">
             <div class="students-text">
-                <h3>${student.name}</h3>
+                <h3 class="students-name">${student.name}</h3>
+                <p class="students-alternate">${student.alternate_names}</p>
+                <p class="students-house">${student.house}</p>
+                <p class="students-birth">${student.dateOfBirth}</p>
+                <a class="more-info">Більше інформації</a>
             </div>
-        `;
+            `;
+        studentsCard.style.backgroundImage = `url(${student.image})`
         studentsContainer.appendChild(studentsCard);
+
+        const imageUrl = student.image && student.image.trim() !== ''
+        ? student.image
+        : 'https://placehold.co/600x400/png';
+
+        studentsCard.style.backgroundImage = imageUrl
     });
 }
+
+// async function faceStudent() {
+//     const card = document.querySelectorAll(".student-card");
+//     let timer = -1
+//     let fetchStudentImage = await InfoRavenclawAndCharactersHouse()
+//     console.log(fetchStudentImage)
+// }
+// faceStudent()
 
 renderRavenclawStudents();
 
